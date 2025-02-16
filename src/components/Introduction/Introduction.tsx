6cf573f6787608d408c0f2e34e1090be93f1bc29
Manu Sisodia <109756813+manusisodia@users.noreply.github.com>
2025-02-16 13:09:00 +0530

    Update Introduction.tsx

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -31,7 +31,7 @@ const Introduction = () => {
           <img src='https://res.cloudinary.com/dhbiouaym/image/upload/v1663667972/Portfolio/logo-s_fna9e6.png' className='intro__logos' alt='' />
           <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={19} />
         </h1>
-        <h2 className='intro__headingSecondary'>Mern Stack Developer / Competitive Programming</h2>
+        <h2 className='intro__headingSecondary'>Full Stack Developer / Competitive Programming</h2>
         <Link to='/contact' className='intro__button'>
           Contact Me
         </Link>
